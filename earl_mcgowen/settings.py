c5fc23f43ce65cf6b3072d2a4fe26d70f1ecb552
Earl McGowen <mcgowee@gmail.com>
2023-12-31 15:14:10 -0700

    first commit

--- earl_mcgowen/settings.py
+++ earl_mcgowen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-2cprhd@*x1apcuu-g32fj5%r-o8ygjmh%3dx*$#mgf(y%s)bb(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mcgowee.pythonanywhere.com']
 
 
 # Application definition
@@ -123,3 +124,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like you don't have local_settings.py file")
+    
