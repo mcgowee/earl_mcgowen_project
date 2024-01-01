ff76d1c8a6d5ce3cc9044770cb40be3f186bcc94
Earl McGowen <mcgowee@gmail.com>
2023-12-31 23:03:06 -0700

    push

--- pages/views.py
+++ pages/views.py
@@ -4,9 +4,10 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import include, path
 from pages import views
+from .models import Page
 
 def home(request):
-    return render(request=request, template_name="pages/home.html")
+    return render(request, "pages/home.html")
 # Create your views here.
 
 # Compare this snippet from pages/urls.py:
